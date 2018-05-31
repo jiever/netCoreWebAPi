99cfc1cbfafe25d6e08a48bf48c400563f9fd4ea
jiever <1090778224@qq.com>
2018-05-31 18:15:54 +0800

    add picture model

---
+++ Project.Model/Picture.cs
@@ -0,0 +1,16 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [SugarTable("cm_pictures")]
+    public class Picture :BaseField
+    {
+
+    }
+}
